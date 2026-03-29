f114aab3389f67a714e57d9c9b2c13026f06d483
Archana ES <emailarchanaes@gmail.com>
2026-03-29 22:51:31 +0530

    Added Selenium locators and basic automation flow

---
+++ src/main/java/com/archana/Main.java
@@ -0,0 +1,42 @@
+package com.archana;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        //WebDriver driver = new FirefoxDriver();
+
+
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
+        //Thread.sleep(10000);
+        //implicit wait
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+        driver.findElement(By.id("inputUsername")).sendKeys("contact@rahulshettyacademy.com");
+        driver.findElement(By.name("inputPassword")).sendKeys("admin123");
+        driver.findElement(By.className("signInBtn")).click();
+        System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
+        driver.findElement(By.linkText("Forgot your password?")).click();
+        driver.findElement(By.xpath("//input[@placeholder='Name']")).sendKeys("Archana");
+        driver.findElement(By.cssSelector("input[placeholder='Email']")).sendKeys("test@gmail.com");
+        driver.findElement(By.cssSelector("input[placeholder='Email']")).clear();
+        driver.findElement(By.xpath("//input[@type='text'][2]")).sendKeys("archanaelavumkudy@gmail.com");
+        driver.findElement(By.cssSelector("input[type='text']:nth-child(4)")).sendKeys("1234567890");
+        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.findElement(By.xpath("//button[2]")).click();
+        //driver.quit();
+
+    }
+}
