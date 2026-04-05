7af2bab27585b1483750f8567e334f0ccc7d76e0
Archana ES <emailarchanaes@gmail.com>
2026-04-05 18:41:24 +0530

    Implemented dynamic password extraction using string split, improved login flow and navigation handling

--- src/main/java/MoreLocators.java
+++ src/main/java/MoreLocators.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-
 import java.time.Duration;
 
 public class MoreLocators {
@@ -14,15 +13,17 @@ public class MoreLocators {
         String name = "Archana";
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("https://rahulshettyacademy.com/locatorspractice/");
 
-
+        String password = getPassword(driver);
+        //driver.get("https://rahulshettyacademy.com/locatorspractice/");
         driver.findElement(By.id("inputUsername")).sendKeys(name);
-        driver.findElement(By.name("inputPassword")).sendKeys("rahulshettyacademy");
-        driver.findElement(By.cssSelector("button[class*='submit']")).click();
+        driver.findElement(By.name("inputPassword")).sendKeys(password);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button[class*='submit']"))).click();
+        //driver.findElement(By.cssSelector("button[class*='submit']")).click();
 
         By messageLocator = By.xpath("//p[text()='You are successfully logged in.']");
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        //WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(messageLocator));
         String message = element.getText();
         System.out.println(message);
@@ -33,5 +34,19 @@ public class MoreLocators {
         Assert.assertEquals(driver.findElement(By.cssSelector("h2")).getText(), "Hello " +name+",");
         driver.findElement(By.xpath("//button[text()='Log Out']")).click();
         driver.quit();
+    }
+
+    public static String getPassword(WebDriver driver){
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
+        driver.findElement(By.linkText("Forgot your password?")).click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.elementToBeClickable(By.className("reset-pwd-btn"))).click();
+        String passwordText = driver.findElement(By.className("infoMsg")).getText();
+        //String[] passwdarr = passwordText.split("'");
+        //String password = passwdarr[1].split("'")[0];
+        String password = passwordText.split("'")[1];
+        driver.findElement(By.xpath("//button[text()='Go to Login']")).click();
+        return password;
+
     }
 }
